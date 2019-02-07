3b0f5b1d8d2eba369a2d758f19ac81bdcfebbfbc
shalu-007 <sunindus.sk@gmail.com>
2019-02-07 11:02:47 +0530

    first commit

---
+++ app/src/main/java/com/example/introapp/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.example.introapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import static android.os.SystemClock.sleep;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen_layout);
+
+        Thread splashThread=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                     sleep(2000);
+                Intent myIntent=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(myIntent);
+                finish();
+            }
+        });
+        splashThread.start();
+    }
+}
